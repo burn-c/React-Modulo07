1a4972ec0c9198951bf1deb10deb7a906a456ded
burn-c <chopcsom@gmail.com>
2019-11-15 20:33:01 -0300

    Styles: Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,115 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/kit-3-pares-de-sapatenis-sapatofran-sw-masculino/56/HAP-0151-256/HAP-0151-256_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
